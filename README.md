efdfffd9c1e8969b8ee9c6cdac0057fff6a8eb48
Tastaturtaste <joshua.moellers@gmx.net>
2023-02-13 18:37:36 +0100

    Added secondary method to find the path to the matlab installation by trying to run the matlab command 'matlabroot' from the commandline.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 matlab-sys provides low level bindings to Matlabs C API to allow writing [MEX functions](https://de.mathworks.com/help/matlab/call-mex-files-1.html?s_tid=CRUX_lftnav) in Rust while using the [C Matrix API](https://de.mathworks.com/help/matlab/cc-mx-matrix-library.html?s_tid=CRUX_lftnav) and interacting with the [C MAT API](https://de.mathworks.com/help/matlab/matlab-c-api-to-read-mat-file-data.html?s_tid=CRUX_lftnav).  
 
 ## Usage
-To make use of the functionality provided by this crate it has to be linked against the library files 'libmex.lib', 'libmx.lib' and 'libmat.lib' provided by your installation of Matlab. To find these libraries define the environment variable 'MATLABPATH' with the path to your Matlab installation as its value.  
+To make use of the functionality provided by this crate it has to be linked against the library files 'libmex.lib', 'libmx.lib' and 'libmat.lib' provided by your installation of Matlab. If the environment variable `MATLABPATH` is set to the installation directory of Matlab to link against this path will be used preferentially. Otherwise the build script will attempt to seek the installation directory of the default matlab installation by running the following command on the build machine: `matlab -batch "disp(matlabroot)"`. This usually takes a few seconds for Matlab to start up, so explicitly providing the path in the aforementioned environment variable helps compile times and should be preferred.
 
-To build a mex function in Rust the crate type has to be 'cdylib', so a dynamic link library. The entry point for a mex function is a function with the signature 
+To build a mex function in Rust the crate type has to be set to `cdylib`, so a dynamic link library. The entry point for a mex function is a function with the signature 
 ```
 #[no_mangle]
 pub unsafe extern "C" fn mexFunction(
@@ -19,9 +19,11 @@ After building the build artifact can simply be renamed into a *.mexw64 for wind
 
 ## Features
 ### separate-complex  
-This feature enables the separate complex storage representation for complex numbers, which is the only available option for Matlab versions 2017b and prior. Since Matlab internally as well as many mathematical libraries use an interleaved complex representation this can incur unnessecary copying. [Complex Storage Documentation]
+This feature enables the separate complex storage representation for complex numbers, which is the only available option for Matlab versions 2017b and prior. Since Matlab internally as well as many mathematical libraries use an interleaved complex representation this can incur unnessecary copying.  
+[Complex Storage Documentation]
 ### interleaved-complex - default  
-This feature enables the interleaved complex storage representation for complex numbers, which is available since Matlab 2018a Update 3. Since many mathematical libraries use an interleaved complex representation this can potentially help avoid copies. This API is slated to become the default for Matlabs mex command as well. [Complex Storage Documentation]
+This feature enables the interleaved complex storage representation for complex numbers, which is available since Matlab 2018a Update 3. Since many mathematical libraries use an interleaved complex representation this can potentially help avoid copies. This API is slated to become the default for Matlabs mex command as well.  
+[Complex Storage Documentation]
 ### link-lib - default  
 This feature enables automatically linking to the necessary libraries in your Matlab installation. If you need more control over this process you can disable this feature by disabling the default features.
 
--- matlab-sys/build.rs
+++ matlab-sys/build.rs
@@ -1,25 +1,14 @@
-use std::env;
+//! Currently all environment variables are assumed to be and contain valid utf-8. If this does not apply to your use-case please consider reporting this as an issue at https://github.com/Tastaturtaste/matlab-sys/issues.
+//! In the meantime this build script can be circumvented by using a cargo .config file with the links key. https://doc.rust-lang.org/cargo/reference/config.html#targettriplelinks.
 
 fn main() {
-    if env::var("CARGO_FEATURE_LINK_LIB").is_err() {
+    // Check if the link-lib feature is disabled. If it is, the user wants full control and has full responsibility to link to the correct libraries.
+    if std::env::var("CARGO_FEATURE_LINK_LIB").is_err() {
         return;
     }
-    // Currently all environment variables are assumed to be and contain valid utf-8. If this does not apply to your use-case please consider reporting this as an issue at https://github.com/Tastaturtaste/matlab-sys/issues.
-    // In the meantime this build script can be circumvented by using a cargo .config file with the links key. https://doc.rust-lang.org/cargo/reference/config.html#targettriplelinks.
-    let matlabpath = env::var("MATLABPATH").expect(
-        "Environment variable 'MATLABPATH' with path to matlab install directory not found. Make sure to create the environment variable 'MATLABPATH' with the path to the matlab installation.",
-    );
-    // For better error messages check if the path provided by the MATLABPATH environment variable actually exists and is readable.
-    assert!(
-        std::path::Path::new(&matlabpath)
-            .try_exists()
-            .expect(&format!("Cannot check existence of path {matlabpath}")),
-        "The path to the matlab installation does not exist: {matlabpath}"
-    );
-
-    // Tell cargo to look for shared libraries in the specified directory. This is platform specific and currently only windows is supported.
+    // Check which platform we run on.
     // TODO: Generalize for other platforms like linux or mac
-    let platform = match env::var("CARGO_CFG_TARGET_OS")
+    let platform = match std::env::var("CARGO_CFG_TARGET_OS")
         .as_deref()
         .expect("Environment variable 'CARGO_CFG_TARGET_OS' not found.")
     {
@@ -28,11 +17,24 @@ fn main() {
         "macos" => OS::MacOS,
         unsupported_target => panic!("Target {unsupported_target} are currently unsupported."),
     };
+
+    let matlabpath = get_matlab_path();
+
+    // For better error messages check if the path to the Matlab installation actually exists and is readable.
+    assert!(
+        std::path::Path::new(&matlabpath)
+            .try_exists()
+            .expect(&format!("Cannot check existence of path {matlabpath}")),
+        "The path to the matlab installation does not exist: {matlabpath}"
+    );
+
+    // Tell cargo to look for shared libraries in the specified directory. This is platform specific and currently only windows is supported.
     assert_eq!(platform, OS::Windows, "Currently only windows is supported. Please raise an issue at https://github.com/Tastaturtaste/matlab-sys/issues with a description of your environment.");
     let mut link_search_path = format!("{matlabpath}/extern/lib/win64/");
     // This value is defined as the empty string if it is not needed for disambiguation for historical reasons
     // The possible values are
-    let target_env = env::var("CARGO_CFG_TARGET_ENV").expect("'CARGO_CFG_TARGET_ENV' not found");
+    let target_env =
+        std::env::var("CARGO_CFG_TARGET_ENV").expect("'CARGO_CFG_TARGET_ENV' not found");
     match target_env.as_str() {
         "msvc" => link_search_path.push_str("microsoft/"),
         "gnu" => link_search_path.push_str("mingw64/"),
@@ -80,6 +82,24 @@ fn add_link_lib_extension(name: impl Into<String>, platform: OS) -> String {
         OS::MacOS => name.push_str(".dylib"),
     }
     name
+}
+
+// Get the path to the matlab installation to link against. Prioritize an explicitly set path, otherwise try to run Matlab and ask it for its directory.
+fn get_matlab_path() -> String {
+    if let Ok(path) = std::env::var("MATLABPATH") {
+        path
+    } else if let Ok(cmd_output) = std::process::Command::new("matlab")
+        .arg("-batch")
+        .arg("disp(matlabroot)")
+        .output()
+    {
+        String::from_utf8(cmd_output.stdout)
+            .expect("The path to the Matlab installation is not valid utf-8")
+            .trim() // Strip the newline matlab appends when using the disp() function
+            .to_owned()
+    } else {
+        panic!("Matlab installation to link against not found. Specify the path to the installation to link against in the environment variable 'MATLABPATH' or make sure Matlab is callable from the command line.")
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]

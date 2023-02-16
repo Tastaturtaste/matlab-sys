0c859a36b2437fabed05272930bc649e86ae762f
Tastaturtaste <joshua.moellers@gmx.net>
2023-02-16 12:09:53 +0100

    Added array-size example to tests.

--- xtask/src/test.m
+++ xtask/src/test.m
@@ -7,3 +7,8 @@ assert(isequal(array_product(5,[2,3,5,7]), [10,15,25,35]));
 %% Test mex_call_matlab
 mex_call_matlab();
 assert(true);
+
+%% Test array_size
+required_size = array_size(10);
+expected_size = 100 / 1024; % In kibibyte
+assert(required_size == expected_size);

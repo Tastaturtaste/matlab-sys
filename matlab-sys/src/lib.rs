0edda820ff4517e424b309edf2e57c354d703dff
Tastaturtaste <joshua.moellers@gmx.net>
2023-02-22 19:21:47 +0100

    Fixed README include Cargo could not find the README which gets included in matlab-sys when packaging, because the relative path to the README lead outside the package. A symlink pointing to the README inside of the package was added and the relative include path changed to point to this symlink.

--- matlab-sys/src/lib.rs
+++ matlab-sys/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"),"/../README.md"))]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"),"/README.md"))]
 
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("The bindings are only valid for 64-bit applications. All Matlab versions after 2015b are only available in 64-bit.");
